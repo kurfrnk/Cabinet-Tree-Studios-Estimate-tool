94eab5aa9ff6e59a80b66eaeb2fe45780c120873
jay <At0104@protonmail.com>
2020-07-26 02:11:27 -0500

    a test

--- client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-
-import { routerReducer as routing } from 'react-router-redux'
-import { combineReducers } from 'redux'
-import cost from './cost'
-
-export default combineReducers({
-  routing,
-  cost
-})
